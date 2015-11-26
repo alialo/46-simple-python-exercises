72d9ae71b7133ea73c8432fec45553c46b43ef2a
Alan Longstaff <alan@alanlongstaff.com>
2015-11-26 21:51:05 +0000

    Added the exercise description into the max.py file (and actually did one it asks...)

--- very simple exercises/max.py
+++ very simple exercises/max.py
@@ -1,8 +1,12 @@
+"""Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Python. (It is true that Python has the max() function built in, but writing it yourself is nevertheless a good exercise.)"""
+
 def max(one,two):
   if one > two:
     print "%d is greater than %d." % (one,two)
+    return one
   elif one < two:
     print "%d is greater than %d." % (two,one)
+    return two
   else:
     print "Your numbers are the same."
 
