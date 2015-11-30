10352ae9c80bf4c852963c89f3d9a1438e6b8a15
Alan Longstaff <alan@alanlongstaff.com>
2015-11-30 22:39:57 +0000

    Added string_length (task 3) solution

---
+++ very simple exercises/string_length.py
@@ -0,0 +1,10 @@
+"""Define a function that computes the length of a given list or string. (It is true that Python has the len() function built in, but writing it yourself is nevertheless a good exercise.)"""
+
+def string_length(x):
+  count = 0
+  for i in x:
+    count += 1
+  return count
+
+result = string_length("this-length-isn't-19")
+print result
